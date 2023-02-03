ac2c577affd881ed802c1e6dd4b2b25e482c6562
Bartosz Majewski <majewskibartosz@pm.me>
2023-02-03 21:56:38 +0100

    create image.ts file

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
+"use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+exports.__esModule = true;
+var styled_components_1 = require("styled-components");
 /* eslint react/prop-types: 0 */
-
-const Img = styled.img`
-  width: 100%;
-  height: 100%;
-  vertical-align: bottom;
-  object-fit: cover;
-  background-color: black;
-`
-
-const Image = ({ src }) => <Img src={src} />
-
-export default Image
+var Img = styled_components_1["default"].img(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  width: 100%;\n  height: 100%;\n  vertical-align: bottom;\n  object-fit: cover;\n  background-color: black;\n"], ["\n  width: 100%;\n  height: 100%;\n  vertical-align: bottom;\n  object-fit: cover;\n  background-color: black;\n"])));
+var Image = function (_a) {
+    var src = _a.src;
+    return src;
+}, src = (void 0).src;
+/>;
+exports["default"] = Image;
+var templateObject_1;
---
+++ src/components/Image.ts
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+/* eslint react/prop-types: 0 */
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  vertical-align: bottom;
+  object-fit: cover;
+  background-color: black;
+`
+
+const Image = ({ src }) => <Img src={ src } />
+
+export default Image
