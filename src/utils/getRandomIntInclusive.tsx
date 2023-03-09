75982ec20176824299f5afbfdde843af951b8742
majewskibartosz <majewskibartosz@pm.me>
2023-03-09 18:18:17 +0100

    fix getRandomIntInclusive compiler errors

--- src/utils/getRandomIntInclusive.tsx
+++ src/utils/getRandomIntInclusive.tsx
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types';
-
 interface GetRandomIntInclusiveProps {
   min: number;
   max: number;
@@ -9,11 +7,6 @@ const getRandomIntInclusive = ({ min, max }: GetRandomIntInclusiveProps): number
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-getRandomIntInclusive.propTypes = {
-  min: PropTypes.number.isRequired,
-  max: PropTypes.number.isRequired,
 };
 
 export default getRandomIntInclusive;
