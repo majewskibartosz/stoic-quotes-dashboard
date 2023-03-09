6b7027f9dbdda9b1f0d0fe337402bc0aab742348
majewskibartosz <majewskibartosz@pm.me>
2023-03-09 18:02:28 +0100

    fix compiler errors in getGreeting component

--- src/utils/getGreeting.tsx
+++ src/utils/getGreeting.tsx
@@ -1,30 +1,25 @@
-import PropTypes from 'prop-types';
-import { mantras, getMantra } from './getMantra'
-import getRandomIntInclusive from './getRandomIntInclusive'
+import { mantras, getMantra } from './getMantra';
+import getRandomIntInclusive from './getRandomIntInclusive';
 
 interface Props {
   time: number;
 }
 
-const getGreeting = ({time}: Props) => {
-  let index = null
+const getGreeting = ({ time }: Props): string => {
+  let index = null;
   if (time <= 12) {
-    index = 0
+    index = 0;
   } else if (time <= 17) {
-    index = 1
+    index = 1;
   } else {
-    index = 2
+    index = 2;
   }
 
-  const greeting = ['Good morning.', 'Good afternoon.', 'Good evening.']
-  const currentMantra = getMantra(mantras)
-  const randomNum = getRandomIntInclusive(1, 6)
+  const greeting = ['Good morning.', 'Good afternoon.', 'Good evening.'];
+  const currentMantra = getMantra(mantras);
+  const randomNum = getRandomIntInclusive({ min: 1, max: 6 });
 
-  return randomNum <= 3 ? greeting[index] : currentMantra
-}
+  return randomNum <= 3 ? greeting[index] : currentMantra;
+};
 
-getGreeting.propTypes = {
-  time: PropTypes.number.isRequired,
-}
-
-export default getGreeting
+export default getGreeting;
