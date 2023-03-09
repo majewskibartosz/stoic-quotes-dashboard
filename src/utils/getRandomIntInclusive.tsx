fa70e5254c16743f091344d83e1a3ff5ff461c9a
majewskibartosz <majewskibartosz@pm.me>
2023-03-09 17:58:27 +0100

    add proptypes and interface to getRandomIntInclusive utility component

--- src/utils/getRandomIntInclusive.tsx
+++ src/utils/getRandomIntInclusive.tsx
@@ -1,8 +1,19 @@
-/* eslint-disable no-param-reassign */
-const getRandomIntInclusive = (min, max) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min
+import PropTypes from 'prop-types';
+
+interface GetRandomIntInclusiveProps {
+  min: number;
+  max: number;
 }
 
-export default getRandomIntInclusive
+const getRandomIntInclusive = ({ min, max }: GetRandomIntInclusiveProps): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+getRandomIntInclusive.propTypes = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+};
+
+export default getRandomIntInclusive;
