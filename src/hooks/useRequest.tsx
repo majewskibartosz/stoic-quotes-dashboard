8389913e26949b26cf37ac91dbbebd6618b2124c
Bartosz Majewski <majewskibartosz@pm.me>
2023-03-09 17:06:04 +0100

    add interface, proptypes, change type annotations in useRequest Hook, fix cache utility component with type annotations

--- src/hooks/useRequest.tsx
+++ src/hooks/useRequest.tsx
@@ -1,42 +1,60 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-unused-expressions */
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { readFromCache, writeToCache } from '../utils/cache'
+import { useState, useEffect } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import PropTypes from 'prop-types';
+import { readFromCache, writeToCache } from '../utils/cache';
 
+interface UseRequestProps {
+  url: string;
+  useCache?: boolean;
+}
 
-const useRequest = (url: string, useCache = true) => {
-  const [data, setData] = useState()
-  const [error, setError] = useState()
+interface CacheData {
+  [key: string]: any;
+}
+
+interface UseRequestData {
+  data: CacheData | undefined;
+  loading: boolean;
+  error: any;
+}
+
+const useRequest = ({ url, useCache = true }: UseRequestProps): UseRequestData => {
+  const [data, setData] = useState<CacheData>();
+  const [error, setError] = useState<any>();
 
   const getFreshData = async () => {
     try {
-      const { data: response } = await axios.get(url)
-      useCache && writeToCache(url, response)
-      setData(response)
+      const { data: response }: AxiosResponse = await axios.get(url);
+      useCache && writeToCache(url, response);
+      setData(response);
     } catch (e: any) {
-      setError(e)
+      setError(e);
     }
-  }
+  };
 
   const getCachedData = () => {
-    const cachedData = readFromCache(url)
-    cachedData && setData(cachedData)
-  }
+    const cachedData = readFromCache(url);
+    cachedData && setData(cachedData);
+  };
 
   useEffect(() => {
-    useCache && getCachedData()
-    getFreshData()
+    useCache && getCachedData();
+    getFreshData();
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
-  const loading = !data && !error
+  const loading = !data && !error;
 
   return {
     data,
     loading,
-    error
-  }
-}
+    error,
+  };
+};
 
-export default useRequest
+useRequest.propTypes = {
+  url: PropTypes.string.isRequired,
+  useCache: PropTypes.bool,
+};
+
+export default useRequest;
--- src/utils/cache.tsx
+++ src/utils/cache.tsx
@@ -1,6 +1,22 @@
-const readFromCache = (url) => JSON.parse(localStorage.getItem(url)) || null
+interface CacheData {
+  [key: string]: any;
+}
 
-const writeToCache = (url, data) =>
-  localStorage.setItem(url, JSON.stringify(data))
+interface CacheFunctions {
+  readFromCache: (url: string) => CacheData | null;
+  writeToCache: (url: string, data: CacheData) => void;
+}
+
+const readFromCache: CacheFunctions['readFromCache'] = (url) => {
+  const cachedData = localStorage.getItem(url)
+  return cachedData ? JSON.parse(cachedData) : null
+}
+
+const writeToCache: CacheFunctions['writeToCache'] = (url, data) => {
+  if (url) {
+    localStorage.setItem(url, JSON.stringify(data))
+  }
+}
+
 
 export { readFromCache, writeToCache }
