3086754fb80b36816cb81f410b94c2f2d5b20b6e
majewskibartosz <majewskibartosz@pm.me>
2023-05-17 00:35:14 +0200

    create eslint temporary rule

--- src/hooks/useRequest.tsx
+++ src/hooks/useRequest.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import PropTypes from 'prop-types';
